60da79cbb8ed0a8508be3729d584e71ddf0a28ce
Logan Lochstampfor <loganl@itl-solutions.com>
2024-02-15 13:42:00 -0500

    this is a side mess here

--- chrome_chromedriver_vr_checker.py
+++ chrome_chromedriver_vr_checker.py
@@ -16,15 +16,13 @@ url = "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions
 x , y = "" , ""
 
 
-#def get_chromebrowser_version(x): attempted make this code below a method and broke the program
+#def get_chromebrowser_version(): #attempted make this code below a method and broke the program
 # Get Chrome browser version via subprocess
 output = subprocess.check_output(r'wmic datafile where name="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" get Version /value', shell = True)
 
 # sets chrome browser version to a variable to compare
 x = output.decode('utf-8').strip()
-print(x, "chrome browser")
-    #return x
-
+print(x, "chrome browser")  
 
 # Get Chromedriver version via subprocess; this program assumes chromedriver.exe is in the same folder as the program itself
 def get_chromedriver_version():
@@ -63,6 +61,7 @@ def clean_stringXY_variable(x):
 # this method is searching for any matches that may be present and reports back if a match is found
 def extract_and_compare(url, z):
     try:
+        print("extract_and_compare is now running")
         # Fetch the JSON data from the URL
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception if the request fails
@@ -75,7 +74,6 @@ def extract_and_compare(url, z):
                 print("version_key =", version_key)
                 if version_key == "versions": # Compare the version with the api json tag "versions"
                     print("step 1, versions")
-                    print("version")
                     if "version" in version_value: # Check if "version" is a key in the version_value dictionary
                         print(f"step 2, Version {z} found in the JSON data.")
                         if version_value["version"] == z: # Checking version of google chrome against the value of the json api; havent limited comparisons to 10 chars - will throw off results
@@ -90,8 +88,13 @@ def extract_and_compare(url, z):
                                         url_value = data.get("url")
                                         if url_value:
                                             print(f"Download URL for win64: {url_value}")
-                                        # Add download logic here
-                                        # ...
+                                            # Add download logic here
+                                            try:
+                                                response = requests.get(url)
+                                                filename = "chromedriver"
+                                                with open(filename, 'wb') as file:
+                                                    file.write(response.content)
+                                            except:print("failure to download new chrome driver")
                                         else:
                                             print("No URL found for win64.")
                     else:
@@ -109,6 +112,7 @@ def extract_and_compare(url, z):
 #this method was made based off 'extract_and_compare' as an improvement;it doesnt seem to go any different due to error at "version" key
 def extract_and_compare2(url, z):
     try:
+        print("extract_and_compare2 is now running")
         # Fetch the JSON data from the URL
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception if the request fails
@@ -119,7 +123,16 @@ def extract_and_compare2(url, z):
         # Check the presence of keys in the JSON data
         if "versions" in data:
             print("step 1, versions")
-            if "version" in data["versions"]:
+            print("versions" in data["versions"], "is 'versions' in data from versions?")
+            print("113.0.5672.0" in data["versions"], "is '113.0.5672.0' in data from versions?")
+            print('versions' in data["versions"], "is versions in data from versions?")
+            print('version' in data["versions"], "is version in data from versions?")
+            print('version' in data,"is version in data?")
+            print('revision' in data["versions"], "is revision in data from versions?")
+            print('downloads' in data["versions"], "is downloads in data from versions?")
+            print('chrome' in data["versions"], "is chrome in data from versions?")
+            #print(data["versions"])
+            if 'version' in data:
                 print(f"step 2, 'version' key found in the JSON data.")
                 if data["versions"]["version"] == z:
                     print("step 3, searching for ",z ," as value of the 'version' key")
@@ -137,19 +150,24 @@ def extract_and_compare2(url, z):
                                         print(f"Download URL for win64: {url_value}")
                                         print("step 8, platform win64 found")
                                         # Add download logic here
-                                        # ...
+                                        try:
+                                            response = requests.get(url)
+                                            filename = "chromedriver"
+                                            with open(filename, 'wb') as file:
+                                                file.write(response.content)
+                                        except:print("Failure to download new chromedriver. Failed after step 7.")
                                     else:
-                                        print("Platform is not win64.")
+                                        print("Platform is not win64. Failed after step 6")
                                 else:
-                                    print("No platform or url key found in the chrome dictionary.")
+                                    print("No platform or url key found in the chrome dictionary. Failed after step 5")
                             else:
-                                print("No chrome key found in the downloads dictionary.")
+                                print("No chrome key found in the downloads dictionary. Failed after step 4")
                         else:
-                            print("No downloads key found in the revision dictionary.")
+                            print("No downloads key found in the revision dictionary. Failed after step 3")
                 else:
-                    print(f"Version {z} not found in the JSON data.")
+                    print(f"Version {z} not found in the JSON data. Failed after step 2")
             else:
-                print("No 'version' key found in the versions dictionary. Failed after step 1")
+                print("No 'version' key found in the data. Failed after step 1")
         else:
             print("No 'versions' key found in the JSON data.")
     except requests.RequestException as e:
@@ -157,17 +175,20 @@ def extract_and_compare2(url, z):
 
 # Call the function to set the value global variables and print versions to cmd
 get_chromedriver_version()
-#get_chromebrowser_version(x) this is commented due to failed attempt to make methodat the top of program
-#print(f"Global Chromedriver version: {chromedriver_version}")
 # sets chrome driver version to a simpler variable to clean and compare
 y = chromedriver_version
+
+#this comment cluster pertain to "x" and chromebrowser version being a method than loose code at the top of program
+#print(x,"after calling chrome browser version method when implemented")
+#get_chromebrowser_version() this is commented due to failed attempt to make method at the top of program
+#print(f"Global Chromedriver version: {chromedriver_version}")
 
 
 # This cluster runs the methods for cleaning x and y to 10 digit strings
 cleaned_x = clean_stringXY_variable(x)
-print(cleaned_x,"clean chrome browser variable x")
+#print(cleaned_x,"clean chrome browser variable x")
 cleaned_y = clean_stringXY_variable(y)
-print(cleaned_y,"clean chrome driver variable y")
+#print(cleaned_y,"clean chrome driver variable y")
 
 #for testing path where versions dont match, leave commented otherwise
 cleaned_x = "113.0.5672.0"
@@ -179,11 +200,10 @@ if cleaned_x == cleaned_y:(
 )
 else:  #access api here to find correct version of chrome driver to match the version of chrome browser
     print("Versions are incompatible; proceeding to obtain correct ChromeDriver version")
-       
+    z = cleaned_x 
     #this cluster should make z an acceptable variable type and value for the api comparison; should it be string
     try:       
-        #this was attempt one at trying to get download link from json file
-        z = cleaned_x
+        #this was attempt one at trying to get download link from json file        
         print("z as chromebrowser version id complete")
         extract_and_compare(url, z)
         print("extract_and_compare(url, z) was successful")
@@ -191,11 +211,59 @@ else:  #access api here to find correct version of chrome driver to match the ve
         print("method call failed; extract_and_compare(url, z) was unsuccessful")     
     try:
         #this was attempt two at trying to get download link from json file
-        z = cleaned_x
         extract_and_compare2(url, z)
         print("extract_and_compare2(url, z) was successful")
     except:
         print("method extract_and_compare2(url, z) unsuccessful")
+
+    try: # Fetch the JSON data from the URL
+        print("third attempt to parse json file started")
+        response = requests.get('https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json')
+        response.raise_for_status()  # Raise an exception if the request fails
+
+        # Parse the JSON content
+        data = json.loads(response.content.decode("utf-8"))
+
+        # Now you can access elements in the JSON data
+        for version in data['versions']:            
+            #print(f"Version: {version['version']}")
+            #print(f"Revision: {version['revision']}")
+            #print(f"Downloads: {version['downloads']}")
+            
+            if data[0] == z:
+                print("version found")
+
+            else:
+                print("Version ",z," not found")    
+    except:print("third attempt at json parsing failed")
+
+    try:
+        print("4th attempt to parse json file started")
+        
+    # Fetch the JSON data from the URL
+        print("line 244")
+        response = requests.get('https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json')
+        response.raise_for_status()  # Raise an exception if the request fails
+        print("line 247")
+    # Parse the JSON content
+        data = json.loads(response.content.decode("utf-8"))
+        #print lines to see what data spits up
+        print("line 251")
+        print(data['versions'][0])
+        print("line 263")
+    # Now you can access elements in the JSON data
+        first_version = data['versions'][0]['version']
+        print("first_version = ", first_version)
+        if z == first_version:
+            print(f"Version {z} matches the first version in the JSON data.")
+        else:
+            print(f"Version {z} does not match the first version in the JSON data.")
+
+        if url:
+            print(f"Download URL for version {z}: {url}")
+        else:
+            print(f"No download URL found for version {z}")
+    except:print("4th attempt to parse json file failed")
 
 print("Version Check for Chrome Scraping Setup complete, Press enter to continue")
 #input()
